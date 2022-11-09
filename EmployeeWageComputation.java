24f2962b9449b5b366ac3435f9a3bf1a75f7cc8b
rishika251 <rishikasadineni@gmail.com>
2022-11-09 14:40:03 +0530

    Added: ability to check employee dailywage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,13 +1,12 @@
 package com.bridgelabz.Day8;
 
 public class EmployeeWageComputation {
-	 public static final int FULL_TIME = 0;
+	  public static final int FULL_TIME = 0;
 	    public static final int PART_TIME = 1;
 	    public static final int perHourWage = 400;
 	    public static int totalEmpHr;
 	    public static int totalWorkingDay;
 	    public static double totalWage;
-
 	    public static void main(String[] args) {
 	        //checking attendence
 	        Attendance uc1 = new Attendance();
@@ -15,10 +14,21 @@ public class EmployeeWageComputation {
 
 	        System.out.println();
 
+
+
+	        //daily wage
+	        EmpDailyWageUC2 uc2 = new EmpDailyWageUC2();
+	        double dailyWageFT = uc2.getEmpDailyWage(8);
+	        System.out.println("FULL TIME emplyoee daily wage is " + dailyWageFT + " Rs");
+
+	        double dailyWagePT = uc2.getEmpDailyWage(4);
+	        System.out.println("PART TIME emplyoee daily wage is " + dailyWagePT + " Rs");
+
+	        System.out.println(); // for empty space
+
 	    }
 
 	    //check attendance
-
 	    public static class Attendance extends EmployeeWageComputation {
 	        public void checkAttendance() {
 	            double checkEmpAtt = (int) (Math.floor(Math.random() * 10) % 4);
@@ -31,6 +41,14 @@ public class EmployeeWageComputation {
 	            }
 	        }
 	    }
-	}
 
+
+	    //calculate daily Wage
+	    public static class EmpDailyWageUC2 extends EmployeeWageComputation {
+	        public double getEmpDailyWage(int FullDayHR) {
+	            return perHourWage * FullDayHR;
+	        }
+	    }
+
+	}
 
